4e7412f5fef18abb8ee443a153ae17947238da66
Angus Yuen <angusyuen@Anguss-MacBook-Pro.local>
2022-02-26 11:55:30 +1100

    refactor: Move feature flag constants to d.ts file

--- src/configurations/featureFlagTypes.ts
+++
@@ -1,8 +0,0 @@
-// All these are global types, defined in webpack.dev.js and webpack.prod.js
-// https://stackoverflow.com/questions/44655095/global-variables-are-undefined-in-typescript-coming-from-webpack
-
-// declare statement required to silence typescript
-// as webpack defines global variables
-declare const mock_get_definition: boolean;
-const _mock_get_definition = mock_get_definition;
-export { _mock_get_definition as mock_get_definition };
