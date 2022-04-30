e36c6d92d32132bacc815f99113bf0f20d284bec
Angus Yuen <angusyuen@Anguss-MacBook-Pro.local>
2022-04-30 16:58:20 +1000

    feat: Users can highlight text and hover to get definition

--- src/content/content_script.tsx
+++ src/content/content_script.tsx
@@ -7,6 +7,7 @@ import { CACHE_ACTIONS, CardHeading, DefinitionEntry } from "../types";
 import React from "react";
 import ReactDOM from "react-dom";
 import { API_ACTIONS } from "../api_client/constants";
+import { isTranslationPopupShowing } from "../utils/utils";
 
 const SERVER = "https://zhiwei-tech.com/cgi-bin/ekho2.pl";
 const VOICE = "iflytekXiaomei";
@@ -31,7 +32,7 @@ const div = document.createElement("div");
 div.id = APP_WRAPPER_ID;
 document.body.appendChild(div);
 
-const displayTranslatePopup = (event: any, data: any) => {
+const displayTranslatePopup = (event: MouseEvent, data: any) => {
   const position = "absolute";
   const left: number = window.pageXOffset + event.clientX;
   -100;
@@ -63,6 +64,47 @@ const displayTranslatePopup = (event: any, data: any) => {
   );
 };
 
+/**
+ * Given an event and text, it generates a popup with the translation
+ * at the event location
+ * 
+ * @param e Mouse event which occurred
+ * @param text Text to translate
+ */
+const generateTranslationContent = async (e: MouseEvent, text: string) => {
+  // Put a max length on how much people can translate
+  if (text.length > 10) return;
+
+  if (text.match(REGEX_CHINESE)) {
+    const cardHeading: CardHeading = { traditional: text };
+    // Display popup whichs says loading...
+    displayTranslatePopup(e, { ...cardHeading, searchComplete: false });
+
+    // Displays popup with definitions
+    const generatePopup = async () => {
+      chrome.runtime.sendMessage(
+        { action: CACHE_ACTIONS.GET_CACHE, text: text },
+        (data: DefinitionEntry) => {
+          if (data.traditional) {
+            console.debug(`Hit cache with: ${data.traditional}`);
+            displayTranslatePopup(e, { ...data, searchComplete: true });
+            return;
+          }
+
+          chrome.runtime.sendMessage(
+            { action: API_ACTIONS.GET_DEFINITION, text: text },
+            (data: DefinitionEntry) => {
+              displayTranslatePopup(e, { ...data, searchComplete: true });
+              chrome.runtime.sendMessage({ action: CACHE_ACTIONS.SET_CACHE, data: data });
+            }
+          );
+        }
+      );
+    };
+    await generatePopup();
+  }
+}
+
 // Dismiss the translate popup when we click outside
 // We are using javascript hacks instead of manipulating via React
 // Because I couldn't figure out how to dynamically show/hide
@@ -80,41 +122,40 @@ document.addEventListener("mousedown", (e: MouseEvent) => {
   }
 });
 
+// Translations for hovering on highlighted text
+document.addEventListener('mousemove', async (e: MouseEvent) => {
+  // If the left click is pressed down, down translate the word
+  // because user may not have finished highlighting
+  if (e.buttons == 1) {
+    return;
+  }
+
+  const highlighted = window.getSelection()?.toString();
+  if (!highlighted) return;
+  
+  // This is the rectangle coordinates of the highlighted text
+  const textRectangle = window.getSelection()?.getRangeAt(0).getClientRects();
+  if (!textRectangle) return;
+  
+  for (var i = 0 ; i < textRectangle.length ; i++) {
+    if(e.clientX >= textRectangle[i].left && e.clientX <= textRectangle[i].right &&
+      e.clientY >= textRectangle[i].top  && e.clientY <= textRectangle[i].bottom
+      ) {
+        if (!isTranslationPopupShowing()) {
+          await generateTranslationContent(e, highlighted);
+          break;
+        }
+    }
+  }
+});
+
 document.addEventListener("dblclick", (e: MouseEvent) => {
-  chrome.storage.local.get("appState", (data) => {
+  chrome.storage.local.get("appState", async (data) => {
     if (!data.appState) return;
 
     const selection = window.getSelection();
     if (!selection) return;
 
-    const text = selection.toString();
-    if (text.match(REGEX_CHINESE)) {
-      const cardHeading: CardHeading = { traditional: text };
-      // Display popup whichs says loading...
-      displayTranslatePopup(e, { ...cardHeading, searchComplete: false });
-
-      // Displays popup with definitions
-      const generatePopup = async () => {
-        chrome.runtime.sendMessage(
-          { action: CACHE_ACTIONS.GET_CACHE, text: text },
-          (data: DefinitionEntry) => {
-            if (data.traditional) {
-              console.debug(`Hit cache with: ${data.traditional}`);
-              displayTranslatePopup(e, { ...data, searchComplete: true });
-              return;
-            }
-
-            chrome.runtime.sendMessage(
-              { action: API_ACTIONS.GET_DEFINITION, text: text },
-              (data: DefinitionEntry) => {
-                displayTranslatePopup(e, { ...data, searchComplete: true });
-                chrome.runtime.sendMessage({ action: CACHE_ACTIONS.SET_CACHE, data: data });
-              }
-            );
-          }
-        );
-      };
-      generatePopup();
-    }
+    await generateTranslationContent(e, selection.toString());
   });
 });
